f114e5a0e07f6fca9da56c7f1a548abaf268b9d6
Paul Holser <pholser@alumni.rice.edu>
2017-01-19 15:41:24 -0600

    Only and Also do not apply to type uses

--- core/src/main/java/com/pholser/junit/quickcheck/Also.java
+++ core/src/main/java/com/pholser/junit/quickcheck/Also.java
@@ -6,7 +6,7 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.*;
 
-@Target({ PARAMETER, FIELD, ANNOTATION_TYPE, TYPE_USE })
+@Target({ PARAMETER, FIELD, ANNOTATION_TYPE })
 @Retention(RUNTIME)
 public @interface Also {
     String[] value();
